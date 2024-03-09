98cd94097ec295da1a778e2f715268a095940c52
ShaokangJiang <shj002@ucsd.edu>
2024-03-08 17:07:04 -0800

    update badge and coverage report

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 // vite.config.js / vite.config.ts
 import GithubActionsReporter from 'vitest-github-actions-reporter'
+import { defineConfig } from 'vite';
 
 export default {
-  test: {
-    reporters: process.env.GITHUB_ACTIONS
-      ? ['default', new GithubActionsReporter()]
-      : 'default'
-  }
+    test: {
+        reporters: process.env.GITHUB_ACTIONS
+            ? ['default', new GithubActionsReporter()]
+            : 'default',
+        coverage: {
+            // you can include other reporters, but 'json-summary' is required, json is recommended
+            reporter: ['text', 'json-summary', 'json'],
+            // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+            reportOnFailure: true,
+        }
+    }
 }
